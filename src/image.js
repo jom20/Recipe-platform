86ee4815bbc1ce5fbf1b6741a0c13bee0d1806fc
joma20 <jomalynenriquez03@gmail.com>
2023-05-01 09:49:25 +0800

    add

---
+++ src/image.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import img1 from './image/MexicanChickenTacos.jpg';
+
+function Images() {
+
+    return(
+
+        <div>
+            <img src={img1}alt=""/>
+        </div>
+    )
+}
+
+export default Image;
